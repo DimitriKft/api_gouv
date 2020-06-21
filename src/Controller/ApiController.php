3e51fe4f4641bc79c33f221ac303491facf44c2b
klopfstein <dimitri.kft@gmail.com>
2020-06-21 15:03:18 +0200

    decode

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -4,17 +4,19 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Encoder\DecoderInterface;
 
 class ApiController extends AbstractController
 {
     /**
      * @Route("/listeRegions", name="listeRegions")
      */
-    public function listeRegion()
+    public function listeRegion(DecoderInterface $decoder)
     {
         $mesRegions = file_get_contents('https://geo.api.gouv.fr/regions');
-        dump($mesRegions);
-        die();
+        $mesRegionsTab = $decoder->decode($mesRegions, 'json');
+        // dump($mesRegionsTab);
+        // die();
         return $this->render('api/index.html.twig');
     }
 }
